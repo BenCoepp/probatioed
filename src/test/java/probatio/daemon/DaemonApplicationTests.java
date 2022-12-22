4625945ee5bde709840b7de1e06c7fb0f778ce73
Ben Coepp <bencoepp@gmail.com>
2022-12-22 17:06:09 +0100

    first commit

---
+++ src/test/java/probatio/daemon/DaemonApplicationTests.java
@@ -0,0 +1,13 @@
+package probatio.daemon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaemonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
